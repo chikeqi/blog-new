e9aa2d7e39d82d0c44c4a7e1d18d8d321697099c
chikeqi <chike20@qq.com>
2026-04-30 21:30:42 +0800

    Delete Desktop/blog-new directory

--- Desktop/blog-new/_worker.js
+++
@@ -1,7 +0,0 @@
-﻿export default {
-    async fetch(request, env) {
-        return new Response('Hello from Cloudflare Pages!', {
-            headers: { 'Content-Type': 'text/plain' }
-        });
-    }
-};
